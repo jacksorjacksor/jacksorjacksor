c95f4c5b88e554e2479760009da1c5b3b8f1dcaa
jacksorjacksor <rich@richardjacksonmusic.com>
2021-05-21 09:56:14 +0100

    Button = triangle, random colours

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const number_of_audio_elements = document.querySelectorAll('audio').length;
 const audio_play_button = document.querySelector('#play-button');
+const body = document.querySelector('body');
 let previous_sample_index = 0;
 
-function random_number_gen() {
-  return Math.floor(Math.random() * number_of_audio_elements) + 1;
+function random_number_gen(number) {
+  return Math.floor(Math.random() * number) + 1;
 }
 
 audio_play_button.addEventListener('click', () => {
-  let sample_index = random_number_gen();
+  let sample_index = random_number_gen(number_of_audio_elements);
   while (sample_index === previous_sample_index) {
-    sample_index = random_number_gen();
+    sample_index = random_number_gen(number_of_audio_elements);
   }
   let audio_element_to_play = document.querySelector(`#bell${sample_index}`);
   if (audio_element_to_play.currentTime > 0) {
@@ -17,4 +18,9 @@ audio_play_button.addEventListener('click', () => {
   }
   audio_element_to_play.play();
   previous_sample_index === sample_index;
+
+  let random_color = `rgb(${random_number_gen(255)},${random_number_gen(
+    255
+  )},${random_number_gen(255)})`;
+  body.style.setProperty('background-color', random_color);
 });
