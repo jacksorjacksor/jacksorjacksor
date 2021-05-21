cbf75a4a0073083222c1a0ca097c2a3c12de9de7
jacksorjacksor <rich@richardjacksonmusic.com>
2021-05-21 06:20:37 +0100

    Slightly tidyed up the JS

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-number_of_audio_elements = document.querySelectorAll('audio').length;
-audio_play_button = document.querySelector('#play-button');
+const number_of_audio_elements = document.querySelectorAll('audio').length;
+const audio_play_button = document.querySelector('#play-button');
 
 audio_play_button.addEventListener('click', () => {
   let element_index = Math.floor(Math.random() * number_of_audio_elements) + 1;
-  let audio_element_to_play = document.querySelector(`#bell${element_index}`);
-  audio_element_to_play.play();
+  document.querySelector(`#bell${element_index}`).play();
 });
