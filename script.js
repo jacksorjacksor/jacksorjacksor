15f80a8887e144ad677b078f90237648ee74b1d4
jacksorjacksor <rich@richardjacksonmusic.com>
2021-05-21 06:33:35 +0100

    Better JS

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const number_of_audio_elements = document.querySelectorAll('audio').length;
 const audio_play_button = document.querySelector('#play-button');
+let previous_sample_index = 0;
+
+function random_number_gen() {
+  return Math.floor(Math.random() * number_of_audio_elements) + 1;
+}
 
 audio_play_button.addEventListener('click', () => {
-  let element_index = Math.floor(Math.random() * number_of_audio_elements) + 1;
-  document.querySelector(`#bell${element_index}`).play();
+  let sample_index = random_number_gen();
+  while (sample_index === previous_sample_index) {
+    sample_index = random_number_gen();
+  }
+  let audio_element_to_play = document.querySelector(`#bell${sample_index}`);
+  if (audio_element_to_play.currentTime > 0) {
+    audio_element_to_play.currentTime = 0;
+  }
+  audio_element_to_play.play();
+  previous_sample_index === sample_index;
 });
