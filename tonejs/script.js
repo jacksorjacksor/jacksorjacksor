bd84d1cd2fd35d063071f5bf6064136b8e6dd442
jacksorjacksor <rich@richardjacksonmusic.com>
2021-06-09 10:32:56 +0100

    Added Mini Metro transcript

--- tonejs/script.js
+++ tonejs/script.js
@@ -1,11 +1,13 @@
-const playButton = document.querySelector('#playButton');
-const synth = new Tone.Synth().toDestination();
-const now = Tone.now();
-
-function sayHi() {
-  console.log('Hi!');
-  // trigger the attack immediately
-  synth.triggerAttackRelease('C4', '8n', now);
-}
-
-playButton.addEventListener('click', sayHi);
+// create two monophonic synths
+const synthA = new Tone.FMSynth().toDestination();
+const synthB = new Tone.AMSynth().toDestination();
+//play a note every quarter-note
+const loopA = new Tone.Loop((time) => {
+  synthA.triggerAttackRelease('C2', '16n', time);
+}, '4n').start(0);
+//play another note every off quarter-note, by starting it "8n"
+const loopB = new Tone.Loop((time) => {
+  synthB.triggerAttackRelease('C4', '8n', time);
+}, '4n').start('8n');
+// all loops start until the Transport is started
+Tone.Transport.start();
